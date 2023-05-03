5e6456c5811bf9536543b5518720a2a02e942207
ViperKelly <49590726+ViperKelly@users.noreply.github.com>
2023-05-02 21:41:05 -0500

    Revert "Webpage Finished?"
    This reverts commit 066ed6fd653bd0e4c97859bc7f43f1dbf9952a11.

---
+++ Camera_test/get_measurables.py
@@ -0,0 +1,6 @@
+from get_image import *
+
+image_width = w
+image_height = h
+
+def click(event, x, y, flags)
--- glass_refactored/glassy/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class GlassyConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'glassy'
